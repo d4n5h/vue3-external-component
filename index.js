66dc44c300a3a137eca943d8a7ebcb52d90c303a
Daniel Shlomo <dshlomo4@gmail.com>
2023-09-19 23:28:00 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { defineAsyncComponent } from "vue";
+import { loadModule } from 'vue3-sfc-loader'
+
+export function loadComponent(url) {
+    return defineAsyncComponent(() => loadModule(url, {
+        moduleCache: {
+            vue: Vue,
+        },
+        async getFile(url) {
+            // Get file from url
+            const res = await fetch(url);
+            if (!res.ok)
+                throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+            return {
+                getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+            }
+        },
+        addStyle(textContent) {
+            // Add CSS to the DOM by creating a <style> tag and inserting textContent into it.
+            const style = Object.assign(document.createElement('style'), { textContent });
+            const ref = document.head.getElementsByTagName('style')[0] || null;
+            document.head.insertBefore(style, ref);
+        },
+    }))
+}
